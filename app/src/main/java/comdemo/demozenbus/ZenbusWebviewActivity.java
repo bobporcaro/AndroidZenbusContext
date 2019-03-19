559da070c2d81248854dea0f169490270313f523
Unknown <boris.raimbault.37@gmail.com>
2019-03-19 17:34:34 +0100

    Add an other webview activity to load zenbus web

--- app/src/main/java/comdemo/demozenbus/ZenbusWebviewActivity.java
+++ app/src/main/java/comdemo/demozenbus/ZenbusWebviewActivity.java
@@ -1,4 +1,19 @@
 package comdemo.demozenbus;
 
-public class ZenbusWebviewActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ZenbusWebviewActivity extends Activity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://zenbus.net/opentourparis");
+    }
 }
